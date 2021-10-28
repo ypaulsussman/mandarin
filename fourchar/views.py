fedfd38ee5ffe4cbcd378be2871892275bdd55cc
Y Sussman <ypaulsussman@gmail.com>
2021-10-28 14:59:43 -0500

    Add search route

--- fourchar/views.py
+++ fourchar/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the chengyu index.")
+  return HttpResponse("Hello, world. You're at the chengyu index. Later, you should add a search form here.")
+
+def search(request, query):
+  return HttpResponse("You sent the following to the server: %s." % query)
