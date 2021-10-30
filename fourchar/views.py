57e60a69a38d07982439d904cfb493f89dfa03c4
Y Sussman <ypaulsussman@gmail.com>
2021-10-30 11:47:47 -0500

    add search-page HTML

--- fourchar/views.py
+++ fourchar/views.py
@@ -1,18 +1,22 @@
-from django import template
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import context, loader
+from django.shortcuts import get_list_or_404, render
 
 from .models import Chengyu
 
+
 def index(request):
-  most_common_chengyus = Chengyu.objects.exclude(chengyu_translation='').order_by('z_rarity')[:5]
-  template = loader.get_template('fourchar/index.html')
-  context = {
-    'most_common_chengyus': most_common_chengyus,
-  }
-  return HttpResponse(template.render(context, request))
-  # return HttpResponse("Hello, world. You're at the chengyu index. Later, you should add a search form here.")
+    context = {
+        'most_common_chengyus': Chengyu.objects.exclude(chengyu_translation='').order_by('z_rarity')[:5],
+    }
+    return render(request, 'fourchar/index.html', context)
+
 
 def search(request, query):
-  return HttpResponse("You sent the following to the server: %s." % query)
+    return render(
+        request,
+        'fourchar/search.html',
+        {
+            'matching_chengyus': Chengyu.objects.filter(chengyu_text__contains=query),
+            'query': query
+        }
+    )
