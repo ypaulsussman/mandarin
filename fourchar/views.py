ccb872c51b01e03c3c6e06ae1ca7cd65e589b7ae
Y Sussman <ypaulsussman@gmail.com>
2021-10-30 14:37:30 -0500

    add AWS Translations

--- fourchar/views.py
+++ fourchar/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import get_list_or_404, render
-
+from django.shortcuts import render
 from .models import Chengyu
+
+import os
+import boto3
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def index(request):
@@ -13,11 +16,30 @@ def index(request):
 
 def search(request):
     query_string = request.GET.get('q')
+    matching_chengyus = Chengyu.objects.filter(
+        chengyu_text__contains=query_string
+    )
+    decorated_chengyus = []
+    client = boto3.client(
+        'translate',
+        aws_access_key_id=os.environ["ACCESS_KEY"],
+        aws_secret_access_key=os.environ["SECRET_KEY"],
+    )
+
+    for chengyu in matching_chengyus:
+        response = client.translate_text(
+            Text=chengyu.chengyu_text,
+            SourceLanguageCode='zh',
+            TargetLanguageCode='en'
+        )
+        chengyu.chengyu_translation = response['TranslatedText']
+        decorated_chengyus.append(chengyu)
+
     return render(
         request,
         'fourchar/search.html',
         {
-            'matching_chengyus': Chengyu.objects.filter(chengyu_text__contains=query_string),
+            'matching_chengyus': decorated_chengyus,
             'query': query_string
         }
     )
